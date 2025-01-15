dd7f3042a5df9a57c5fd00958093173caf81f18c
itscrystalline <pvpthadgaming@gmail.com>
2025-01-15 10:23:00 +0700

    nyuushutsuryokusha: export mechanism scaffold

--- src/nyuushutsuryokusha.rs
+++ src/nyuushutsuryokusha.rs
@@ -3,7 +3,7 @@ use env_logger::Env;
 use log::{error, info};
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use rusqlite::Error;
 use serde::{Deserialize, Serialize};
 mod libfukushuu;
@@ -14,13 +14,23 @@ use crate::libfukushuu::db::{Card, Category, Pool};
 #[command(name = "入出力者 (Nyūshutsuryokusha)")]
 #[command(version, about, long_about = None)]
 struct Args {
+    #[command(subcommand)]
+    command: Commands,
+
     #[arg(short, long, default_value = "info")]
     log_level: String,
     #[arg(short, long, default_value = "false")]
     refresh_db: bool,
     #[arg(short, long, value_name = "FILE", default_value = "flashcards.db")]
     db: Option<PathBuf>,
-    from: Option<PathBuf>,
+
+    json: Option<PathBuf>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Import,
+    Export,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -50,7 +60,7 @@ fn main() {
     let args = Args::parse();
     env_logger::Builder::from_env(Env::default().default_filter_or(args.log_level)).init();
 
-    let from_file = match args.from {
+    let json_file = match args.json {
         Some(f) => f,
         None => {
             error!("{}", "From file not specified!".red());
@@ -66,7 +76,7 @@ fn main() {
     };
     info!(
         "{}",
-        format!("Reading from file {:?} to db at {:?}", from_file, db_path).cyan()
+        format!("File at {:?} and Database at {:?}", json_file, db_path).cyan()
     );
     let db = match db::create_or_open(db_path) {
         Ok(d) => d,
@@ -76,7 +86,7 @@ fn main() {
         }
     };
 
-    let json = std::fs::read_to_string(from_file).unwrap();
+    let json = std::fs::read_to_string(json_file).unwrap();
     let content: FukushuuJson = match serde_json::from_str(json.as_str()) {
         Ok(c) => c,
         Err(error) => {
@@ -86,98 +96,110 @@ fn main() {
         }
     };
 
-    info!(
-        "{}",
-        format!(
-            "Importing data... ({} Categories)",
-            content.categories.len()
-        )
-        .blue()
-    );
-
-    macro_rules! check_exists {
-        ($to_match: expr, $err_msg: expr) => {
-            match $to_match {
-                Ok(_) => true,
-                Err(e) => match e {
-                    Error::QueryReturnedNoRows => false,
-                    _ => {
-                        error!("{}", format!($err_msg, e).red());
-                        false
-                    }
-                },
-            }
-        };
-    }
-
-    content.categories.iter().for_each(|category| {
-        info!(
-            "{}",
-            format!(
-                "├ Category: {} ({} Pools)",
-                category.name,
-                category.pools.len()
-            )
-            .blue()
-        );
-        let category_exists = check_exists!(
-            Category::get_one(&db, &category.name),
-            "Error accessing Categories: {}!"
-        );
-        if !category_exists {
-            Category::add(
-                &db,
-                Category {
-                    name: category.name.clone(),
-                },
-            )
-                .unwrap();
-        }
-
-        category.pools.iter().for_each(|pool| {
+    match args.command {
+        Commands::Import => {
             info!(
                 "{}",
-                format!("│ ├ Pool: {} ({} Cards)", pool.id, pool.cards.len()).blue()
-            );
-            let pool_exists =
-                check_exists!(Pool::get_by_id(&db, pool.id), "Error accessing Pools: {}!");
-            if !pool_exists {
-                Pool::add(
-                    &db,
-                    Pool {
-                        id: pool.id,
-                        category_name: Some(category.name.clone()),
-                    },
+                format!(
+                    "Importing data... ({} Categories)",
+                    content.categories.len()
                 )
-                    .unwrap();
+                .blue()
+            );
+
+            macro_rules! check_exists {
+                ($to_match: expr, $err_msg: expr) => {
+                    match $to_match {
+                        Ok(_) => true,
+                        Err(e) => match e {
+                            Error::QueryReturnedNoRows => false,
+                            _ => {
+                                error!("{}", format!($err_msg, e).red());
+                                false
+                            }
+                        },
+                    }
+                };
             }
-            pool.cards.iter().for_each(|card| {
-                if validate_card(card) {
-                    Card::add(&db, Card {
-                        id: None,
-                        front: card.front.clone().unwrap_or_default(),
-                        back: card.back.clone().unwrap_or_default(),
-                        front_image: card.front_image.clone().unwrap_or_default(),
-                        back_image: card.back_image.clone().unwrap_or_default(),
-                        score: None,
-                        pool_id: Some(pool.id),
-                        category_name: Some(category.name.clone()),
-                    }).unwrap();
-                    info!("{} {}", "│ │".blue(), format!("├ Card: {:?}", card).green());
-                } else {
-                    error!(
-                        "{} {}",
-                        "│ │".blue(),
-                        format!(
+
+            content.categories.iter().for_each(|category| {
+                info!(
+                    "{}",
+                    format!(
+                        "├ Category: {} ({} Pools)",
+                        category.name,
+                        category.pools.len()
+                    )
+                    .blue()
+                );
+                let category_exists = check_exists!(
+                    Category::get_one(&db, &category.name),
+                    "Error accessing Categories: {}!"
+                );
+                if !category_exists {
+                    Category::add(
+                        &db,
+                        Category {
+                            name: category.name.clone(),
+                        },
+                    )
+                        .unwrap();
+                }
+
+                category.pools.iter().for_each(|pool| {
+                    info!(
+                        "{}",
+                        format!("│ ├ Pool: {} ({} Cards)", pool.id, pool.cards.len()).blue()
+                    );
+                    let pool_exists =
+                        check_exists!(Pool::get_by_id(&db, pool.id), "Error accessing Pools: {}!");
+                    if !pool_exists {
+                        Pool::add(
+                            &db,
+                            Pool {
+                                id: pool.id,
+                                category_name: Some(category.name.clone()),
+                            },
+                        )
+                            .unwrap();
+                    }
+                    pool.cards.iter().for_each(|card| {
+                        if validate_card(card) {
+                            Card::add(
+                                &db,
+                                Card {
+                                    id: None,
+                                    front: card.front.clone().unwrap_or_default(),
+                                    back: card.back.clone().unwrap_or_default(),
+                                    front_image: card.front_image.clone().unwrap_or_default(),
+                                    back_image: card.back_image.clone().unwrap_or_default(),
+                                    score: None,
+                                    pool_id: Some(pool.id),
+                                    category_name: Some(category.name.clone()),
+                                },
+                            )
+                                .unwrap();
+                            info!("{} {}", "│ │".blue(), format!("├ Card: {:?}", card).green());
+                        } else {
+                            error!(
+                                "{} {}",
+                                "│ │".blue(),
+                                format!(
                             "├ ✘ Card: {:?} (Missing `front`&`front_image` or `back`&`back_image`)",
                             card
                         )
-                        .red().strikethrough()
-                    );
-                }
+                                .red()
+                                .strikethrough()
+                            );
+                        }
+                    });
+                });
             });
-        });
-    });
+        }
+        Commands::Export => {
+            todo!()
+        }
+    }
 
     db::close_db(db).unwrap()
 }
